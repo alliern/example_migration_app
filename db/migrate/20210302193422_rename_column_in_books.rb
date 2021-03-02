636147cfc1aa66f9881df077f8dfaf40b4cd6856
Allie Neal <allieneal93@gmail.com>
2021-03-02 15:31:41 -0500

    adds some # text

--- db/migrate/20210302193422_rename_column_in_books.rb
+++ db/migrate/20210302193422_rename_column_in_books.rb
@@ -1,5 +1,6 @@
 class RenameColumnInBooks < ActiveRecord::Migration[6.1]
   def change
+    # change_column :books, :description, :plot
     change_table :books do |t|
       t.rename :description, :plot
     end
--- db/migrate/20210302193710_change_data_type_in_books.rb
+++ db/migrate/20210302193710_change_data_type_in_books.rb
@@ -1,5 +1,6 @@
 class ChangeDataTypeInBooks < ActiveRecord::Migration[6.1]
   def change
     change_column :books, :isbn, 'integer USING CAST(isbn AS integer)'
+  #   change_column :books, :isbn, :integer, 'isbn::integer'
   end
 end
